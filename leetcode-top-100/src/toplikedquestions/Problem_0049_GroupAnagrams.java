9e054c15365481ab3edd48b7c2ba6463fbcadd20
lidong.han <2290707867@qq.com>
2022-09-28 11:55:58 +0800

    ‘leetcode 49的另一种方法, 后续有时间优化’

--- leetcode-top-100/src/toplikedquestions/Problem_0049_GroupAnagrams.java
+++ leetcode-top-100/src/toplikedquestions/Problem_0049_GroupAnagrams.java
@@ -1,10 +1,7 @@
 package toplikedquestions;
 
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 // 字母异位词分组
 // https://leetcode.cn/problems/group-anagrams/
@@ -29,4 +26,31 @@ public class Problem_0049_GroupAnagrams {
         }
         return result;
     }
+
+//    // 没有使用排序, 用了一个int[26]数组(下标表示字母, num表示字符出现的次数)记录一个str中出现的的次数, 自己通过StringBuild 构造一个区分同情字符串的通用key去判断
+//    public List<List<String>> groupAnagrams(String[] strs) {
+//        List<List<String>> result = new ArrayList<>();
+//        Map<String, List<String>> map = new HashMap<>();
+//        for (String str : strs) {
+//            int[] record = new int[26];  // 统计字符个数
+//            for (char c : str.toCharArray()) {
+//                record[c - 'a']++;
+//            }
+//            StringBuilder sb = new StringBuilder();
+//            for (int value : record) {
+//                sb.append(String.valueOf(value)).append("_");
+//            }
+//            String key = sb.toString(); // dac = "1_0_1_1_0_..."(代表同情字符串, 自己构造了一个判断同情字符串的key)
+//            if (!map.containsKey(key)) {
+//                map.put(key, new ArrayList<>());
+//            }
+//            map.get(key).add(str);
+//        }
+//
+//        // 收集答案
+//        for (List<String> cur : map.values()) {
+//            result.add(cur);
+//        }
+//        return result;
+//    }
 }
